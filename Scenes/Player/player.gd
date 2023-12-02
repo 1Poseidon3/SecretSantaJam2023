e6bc892f0fb9af1092e11fe09c135bdf29bbe04f
Poseidon <ballycroyboy@gmail.com>
2023-12-02 01:21:04 -0500

    Added a basic FPS controller framework

---
+++ Scenes/Player/player.gd
@@ -0,0 +1,76 @@
+extends CharacterBody3D
+
+
+const WALK_SPEED = 5.0
+const SPRINT_SPEED = 7.0
+const JUMP_VELOCITY = 4.5
+const SENSITIVITY = 0.001
+
+var gravity = 9.8 #ProjectSettings.get_setting("physics/3d/default_gravity")
+var t_bob = 0.0
+var speed = 5.0
+
+@onready var head = $Head
+@onready var camera = $Head/Camera3D
+
+@export var look_down_limit = -40
+@export var look_up_limit = 60
+@export var bob_frequency = 2.0
+@export var bob_amplitude = 0.08
+@export var base_fov = 75.0
+@export var fov_change = 1.5
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	$Body.visible = false
+	
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		head.rotate_y(-event.relative.x * SENSITIVITY)
+		camera.rotate_x(-event.relative.y * SENSITIVITY)
+		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(look_down_limit), deg_to_rad(look_up_limit))
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		
+	if Input.is_action_pressed("sprint"):
+		speed = SPRINT_SPEED
+	else:
+		speed = WALK_SPEED
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("left", "right", "forward", "backward")
+	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if is_on_floor():
+		if direction:
+			velocity.x = direction.x * speed
+			velocity.z = direction.z * speed
+		else:
+			velocity.x = lerp(velocity.x, direction.x * speed, delta * 7.0) #move_toward(velocity.x, 0, SPEED)
+			velocity.z = lerp(velocity.z, direction.z * speed, delta * 7.0) #move_toward(velocity.z, 0, SPEED)
+	else:
+		velocity.x = lerp(velocity.x, direction.x * speed, delta * 3.0)
+		velocity.z = lerp(velocity.z, direction.z * speed, delta * 3.0)
+	
+	t_bob += delta * velocity.length() * float(is_on_floor())
+	camera.transform.origin = headbob(t_bob)
+	
+	var velocity_clamped = clamp(velocity.length(), 0.5, SPRINT_SPEED * 2)
+	var target_fov = base_fov + fov_change * velocity_clamped
+	camera.fov = lerp(camera.fov, target_fov, delta * 8.0)
+	
+	move_and_slide()
+	
+func headbob(time) -> Vector3:
+	var pos = Vector3.ZERO
+	pos.y = sin(time * bob_frequency) * bob_amplitude
+	pos.x = sin(time * bob_frequency / 2) * bob_amplitude
+	return pos
+

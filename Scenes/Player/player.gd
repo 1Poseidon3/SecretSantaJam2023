aae1c084ac257132b9dc928362ad68310e091b9c
Poseidon <ballycroyboy@gmail.com>
2023-12-09 02:08:46 -0500

    Added a rudimentary zombie
    It follows the player and it is rigged and has animations but there is no animation tree yet.

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -43,9 +43,7 @@ func _physics_process(delta):
 		speed = SPRINT_SPEED
 	else:
 		speed = WALK_SPEED
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+		
 	var input_dir = Input.get_vector("left", "right", "forward", "backward")
 	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if is_on_floor():
@@ -53,8 +51,8 @@ func _physics_process(delta):
 			velocity.x = direction.x * speed
 			velocity.z = direction.z * speed
 		else:
-			velocity.x = lerp(velocity.x, direction.x * speed, delta * 7.0) #move_toward(velocity.x, 0, SPEED)
-			velocity.z = lerp(velocity.z, direction.z * speed, delta * 7.0) #move_toward(velocity.z, 0, SPEED)
+			velocity.x = lerp(velocity.x, direction.x * speed, delta * 7.0)
+			velocity.z = lerp(velocity.z, direction.z * speed, delta * 7.0)
 	else:
 		velocity.x = lerp(velocity.x, direction.x * speed, delta * 3.0)
 		velocity.z = lerp(velocity.z, direction.z * speed, delta * 3.0)

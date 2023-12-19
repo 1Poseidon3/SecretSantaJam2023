f64ec0ce9e30dbd8b3adefd20c68e3cd79ac99b3
Poseidon <ballycroyboy@gmail.com>
2023-12-18 23:17:42 -0500

    Biggest change added M1911
    Can't remember what else I changed. Kinda in crunch mode here.

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -10,6 +10,8 @@ const HIT_STAGGER = 8.0
 var gravity = 9.8
 var t_bob = 0.0
 var speed = 5.0
+var ammo_reserve = 35
+var ammo_in_gun = 7
 
 signal player_hit
 signal player_ready
@@ -17,6 +19,7 @@ signal player_ready
 @onready var head = $Head
 @onready var camera = $Head/Camera3D
 @onready var raycast = $Head/RayCast3D
+@onready var gun_anim = $Head/Camera3D/Gun.get_child(0).get_child(1)
 
 @export var look_down_limit = -40
 @export var look_up_limit = 60
@@ -39,11 +42,18 @@ func _unhandled_input(event):
 		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(look_down_limit), deg_to_rad(look_up_limit))
 
 func _physics_process(delta):
-	# Add the gravity.
+	if Input.is_action_just_pressed("shoot"):
+		shoot()
+	
+	if Input.is_action_just_pressed("action"):
+		buy()
+	
+	if Input.is_action_just_pressed("reload"):
+		reload()
+	
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
-	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		
@@ -90,3 +100,18 @@ func hit(dir):
 	emit_signal("player_hit")
 	velocity.x += dir.x * HIT_STAGGER
 	velocity.z += dir.z * HIT_STAGGER
+
+func shoot():
+	if ammo_in_gun > 0:
+		if !gun_anim.is_playing():
+			gun_anim.play("Shoot")
+
+func buy():
+	pass
+
+func reload():
+	if !gun_anim.is_playing():
+		if ammo_in_gun > 0:
+			gun_anim.play("Reload Not Empty")
+		else:
+			gun_anim.play("Reload")
